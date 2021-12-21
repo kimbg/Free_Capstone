ed7833236d8dda695a07b9cea6283f30f58752bd
kimbg <rlaqudrhks6@gmail.com>
2021-12-22 02:33:24 +0900

    update

--- Front/script/registerCoords.js
+++ Front/script/registerCoords.js
@@ -10,19 +10,29 @@ $(function() {
     console.log('lat : ',lat)
     console.log('lng : ',lng)
 
-    let newcoords;
-    if(lat != 0 && lng != 0) 
-        newcoords = new kakao.maps.LatLng(lat,lng);
-    else
-        newcoords = new kakao.maps.LatLng(35.24872579785369, 128.9028173515892);
     
-        console.log('newcoords',newcoords);
+    var mapOption;
+    let newcoords = new kakao.maps.LatLng(lng,lat);
+    if(lat != 0 && lng != 0) 
+    {
+         mapOption = { 
+            center: new kakao.maps.LatLng(lng,lat), // 지도의 중심좌표
+            level: 3 // 지도의 확대 레벨
+        };
+    }
+    else //사진에 메타데이터 없는경우
+    {
+        lat = 35.24872579785369;
+        lng = 128.9028173515892;
+        mapOption = { 
+            center: new kakao.maps.LatLng(lng,lat), // 지도의 중심좌표
+            level: 3 // 지도의 확대 레벨
+        };
+    }
+     console.log('mapOption : ',mapOption);
 
     var mapContainer = document.getElementById('map'); // 지도를 표시할 div 
-    var mapOption = { 
-        center: new kakao.maps.LatLng(newcoords.La,newcoords.Ma), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
-    };
+    
 
     var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
@@ -34,8 +44,9 @@ $(function() {
     var latlng;
     kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
     
-        latlng = mouseEvent.latLng; 
-        
+        latlng = mouseEvent.latLng;         
+
+
         marker.setPosition(latlng);
         
         var message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
@@ -49,14 +60,14 @@ $(function() {
 
     function submitCoords() {
         
-        if(newcoords.La != 0 && newcoords.Ma != 0){
+        if(lat != 0 && lng != 0){
             alert("확인");
             $.ajax({
                 url : '/map/submitCoords',
                 type : 'POST',
                 data : {
-                    lat : newcoords.getLat(),
-                    lng : newcoords.getLng(),
+                    lat : lat,
+                    lng : lng,
                 }
             })           
             .done(function(response) {
