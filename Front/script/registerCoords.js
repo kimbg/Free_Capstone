4d4ba173541cf84620cf7d135671f0a59b4b1bf7
kimbg <rlaqudrhks6@gmail.com>
2021-12-21 22:51:52 +0900

    지도 오류 수정

--- Front/script/registerCoords.js
+++ Front/script/registerCoords.js
@@ -11,17 +11,19 @@ $(function() {
     console.log('lat : ',lat)
     console.log('lng : ',lng)
     console.log('comment : ',comment)
+    console.log(typeof(lat),typeof(lng));
+    console.log(typeof(parseFloat(lat)),parseFloat(lat));
 
     let newcoords;
     if(lat != 0 && lng != 0) 
         newcoords = new kakao.maps.LatLng(lat,lng);
     else
         newcoords = new kakao.maps.LatLng(35.24872579785369, 128.9028173515892);
-    console.log(newcoords);
+    console.log('newcoords : ',newcoords);
 
     var mapContainer = document.getElementById('map'); // 지도를 표시할 div 
     var mapOption = { 
-        center: newcoords, // 지도의 중심좌표
+        center: new kakao.maps.LatLng(newcoords.La,newcoords.Ma), // 지도의 중심좌표
         level: 3 // 지도의 확대 레벨
     };
 
@@ -41,7 +43,7 @@ $(function() {
     kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
         
         // 클릭한 위도, 경도 정보를 가져옵니다 
-        latlng = newcoords;
+        latlng = mouseEvent.latLng;
         
         // 마커 위치를 클릭한 위치로 옮깁니다
         marker.setPosition(latlng);
@@ -58,7 +60,6 @@ $(function() {
     function submitCoords() {
         
         if(newcoords.La != 0 && newcoords.Ma != 0){
-            alert("확인");
             $.ajax({
                 url : '/map/submitCoords',
                 type : 'POST',
